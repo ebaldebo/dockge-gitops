bc00bc323316077a9b1a194dc40864aa6fac5be5
Baldebo, Emil <ebaldebo@gmail.com>
2024-01-05 15:43:26 +0100

    move logic from main to app package

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"time"
 
-	"github.com/ebaldebo/dockge-gitops/internal/cmdexecutor"
-	"github.com/ebaldebo/dockge-gitops/internal/env"
-	"github.com/ebaldebo/dockge-gitops/internal/git"
+	"github.com/ebaldebo/dockge-gitops/internal/app"
 )
 
 func main() {
-	gitHubRepoUrl, err := env.GetEnvVar(true, "GITHUB_REPO_URL", "")
-	handleError(err)
-
-	gitHubPAT, err := env.GetEnvVar(false, "GITHUB_PAT", "")
-	handleError(err)
-
-	// pollingRate, err := env.GetEnvVar(false, "POLLING_RATE", "1m")
-	// handleError(err)
-
-	cmdExecutor := &cmdexecutor.DefaultCommandExecutor{}
-
-	gitErr := git.CloneOrPullRepo(cmdExecutor, gitHubRepoUrl, gitHubPAT, "/tmp/repo")
-	handleError(gitErr)
+	app.Run()
 
 	time.Sleep(10 * time.Minute)
-}
-
-func handleError(err error) {
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
 }
---
+++ internal/app/app.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ebaldebo/dockge-gitops/internal/cmdexecutor"
+	"github.com/ebaldebo/dockge-gitops/internal/env"
+	"github.com/ebaldebo/dockge-gitops/internal/git"
+)
+
+func Run() {
+	gitHubRepoUrl, err := env.GetEnvVar(true, "GITHUB_REPO_URL", "")
+	handleError(err)
+
+	gitHubPAT, err := env.GetEnvVar(false, "GITHUB_PAT", "")
+	handleError(err)
+
+	// pollingRate, err := env.GetEnvVar(false, "POLLING_RATE", "1m")
+	// handleError(err)
+
+	cmdExecutor := &cmdexecutor.DefaultCommandExecutor{}
+
+	gitErr := git.CloneOrPullRepo(cmdExecutor, gitHubRepoUrl, gitHubPAT, "/tmp/repo")
+	handleError(gitErr)
+
+	time.Sleep(10 * time.Minute)
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
